dfd9debaad454ef7ec3b37ab14c4afe10a8a42fc
lightrao <lightrao@hotmail.com>
2024-09-07 21:56:53 +0800

    Displaying Category Items on a Screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:fdtcg_multi_screen_navigating_meals_app/data/dummy_data.dart';
+import 'package:fdtcg_multi_screen_navigating_meals_app/widgets/category_grid_item.dart';
+
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
@@ -12,49 +15,21 @@ class CategoriesScreen extends StatelessWidget {
         ),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2, // width / height
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
-        children: const [
-          Text(
-            '1',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-          Text(
-            '2',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-          Text(
-            '3',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-          Text(
-            '4',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-          Text(
-            '5',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-          Text(
-            '6',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
+        // children: availableCategories
+        //     .map(
+        //       (category) => CategoryGridItem(category: category) as Widget,
+        //     )
+        //     .toList(),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(category: category),
         ],
       ),
     );
