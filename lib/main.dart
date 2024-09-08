626dce51ebef10ed7ff3616fc2a6264ce967a3be
lightrao <lightrao@hotmail.com>
2024-09-08 17:20:48 +0800

    Adding Cross-Screen Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'package:fdtcg_multi_screen_navigating_meals_app/screens/categories.dart';
-import 'package:fdtcg_multi_screen_navigating_meals_app/screens/meals.dart';
-import 'package:fdtcg_multi_screen_navigating_meals_app/data/dummy_data.dart';
+// import 'package:fdtcg_multi_screen_navigating_meals_app/screens/meals.dart';
+// import 'package:fdtcg_multi_screen_navigating_meals_app/data/dummy_data.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -26,10 +26,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: MealsScreen(
-        tile: 'Some category ...',
-        meals: dummyMeals,
-      ),
+      home: const CategoriesScreen(),
     );
   }
 }
--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -2,9 +2,22 @@ import 'package:flutter/material.dart';
 
 import 'package:fdtcg_multi_screen_navigating_meals_app/data/dummy_data.dart';
 import 'package:fdtcg_multi_screen_navigating_meals_app/widgets/category_grid_item.dart';
+import 'package:fdtcg_multi_screen_navigating_meals_app/screens/meals.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context) {
+    // Navigator.push(context, route);
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => const MealsScreen(
+          tile: 'Some title',
+          meals: [],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +42,12 @@ class CategoriesScreen extends StatelessWidget {
         //     .toList(),
         children: [
           for (final category in availableCategories)
-            CategoryGridItem(category: category),
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context);
+              },
+            ),
         ],
       ),
     );
