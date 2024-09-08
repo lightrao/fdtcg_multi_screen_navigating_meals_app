083ed83bbb9d4e7259943c2649602b85a08dee77
lightrao <lightrao@hotmail.com>
2024-09-08 09:54:11 +0800

    Loading Meals Data Into a Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
+
 import 'package:fdtcg_multi_screen_navigating_meals_app/screens/categories.dart';
+import 'package:fdtcg_multi_screen_navigating_meals_app/screens/meals.dart';
+import 'package:fdtcg_multi_screen_navigating_meals_app/data/dummy_data.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -23,7 +26,10 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const CategoriesScreen(),
+      home: MealsScreen(
+        tile: 'Some category ...',
+        meals: dummyMeals,
+      ),
     );
   }
 }
