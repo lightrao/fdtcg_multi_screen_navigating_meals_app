5c6f9671baa39748ea7d25811dec7fd3af659aac
lightrao <lightrao@hotmail.com>
2024-09-14 07:42:08 +0800

    Creating a Provider

---
+++ lib/providers/meals_provider.dart
@@ -0,0 +1,8 @@
+import 'package:fdtcg_multi_screen_navigating_meals_app/data/dummy_data.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final mealsProvider = Provider(
+  (ref) {
+    return dummyMeals;
+  },
+);
