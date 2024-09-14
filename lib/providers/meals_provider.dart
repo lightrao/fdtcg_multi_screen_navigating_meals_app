499bec82a31104f87487c0e91dcf0d89de615b33
lightrao <lightrao@hotmail.com>
2024-09-14 12:59:14 +0800

    Using a Provider

--- lib/providers/meals_provider.dart
+++ lib/providers/meals_provider.dart
@@ -1,7 +1,8 @@
 import 'package:fdtcg_multi_screen_navigating_meals_app/data/dummy_data.dart';
+import 'package:fdtcg_multi_screen_navigating_meals_app/models/meal.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final mealsProvider = Provider(
+final mealsProvider = Provider<List<Meal>>(
   (ref) {
     return dummyMeals;
   },
--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -4,7 +4,9 @@ import 'package:fdtcg_multi_screen_navigating_meals_app/screens/categories.dart'
 import 'package:fdtcg_multi_screen_navigating_meals_app/screens/filters.dart';
 import 'package:fdtcg_multi_screen_navigating_meals_app/screens/meals.dart';
 import 'package:fdtcg_multi_screen_navigating_meals_app/widgets/main_drawer.dart';
+import 'package:fdtcg_multi_screen_navigating_meals_app/providers/meals_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 const kInitialFilters = {
   Filter.glutenFree: false,
@@ -13,16 +15,16 @@ const kInitialFilters = {
   Filter.vegan: false,
 };
 
-class TabsScreen extends StatefulWidget {
+class TabsScreen extends ConsumerStatefulWidget {
   const TabsScreen({super.key});
 
   @override
-  State<StatefulWidget> createState() {
+  ConsumerState<TabsScreen> createState() {
     return _TabsScreenState();
   }
 }
 
-class _TabsScreenState extends State<TabsScreen> {
+class _TabsScreenState extends ConsumerState<TabsScreen> {
   int _selectedPageIndex = 0;
   final List<Meal> _favoriteMeals = [];
   Map<Filter, bool> _selectedFilters = kInitialFilters;
@@ -77,7 +79,9 @@ class _TabsScreenState extends State<TabsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final availableMeals = dummyMeals.where((meal) {
+    final meals = ref.watch(mealsProvider);
+
+    final availableMeals = meals.where((meal) {
       if (_selectedFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
         return false;
       }
